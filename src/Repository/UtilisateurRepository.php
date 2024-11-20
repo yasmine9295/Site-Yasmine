2109a641b8eac3973a8550ec73143011c941ff9d
Yasmine EL HARIM <yasmine.elharim@btssio.dedyn.io>
2024-11-20 11:15:22 +0100

    projetg

--- src/Repository/UtilisateurRepository.php
+++ src/Repository/UtilisateurRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Utilisateur;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use App\Repository\UtilisateurRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Utilisateur>
