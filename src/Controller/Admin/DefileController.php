bedb43fc8cc6fcbab53b191eea17d0d9efa9f19a
Manon <manco15@outlook.fr>
2025-01-04 11:18:48 +0100

    barre

--- src/Controller/Admin/DefileController.php
+++ src/Controller/Admin/DefileController.php
@@ -67,23 +67,42 @@ class DefileController extends AbstractController
         return $this->redirectToRoute('admin_defiles');
     }
 
-    public function index(Request $request, DefileRepository $DefileRepository, PaginatorInterface $paginator): Response
-    {
-        $search = $request->query->get('search');
+    public function index(Request $request): Response
+{
+    $search = $request->query->get('search', '');
+    $mannequin = $request->query->get('mannequin', null);
+    $annee = $request->query->get('annee', null);
 
-        $query = $search 
-            ? $DefileRepository->findBySearchQuery($search) 
-            : $DefileRepository->findAllQuery();
-        $lesDefiles = $paginator->paginate(
-            $query, 
-            $request->query->getInt('page', 1), 
-            9 
-        );
+    // Construire la requête pour filtrer les défilés
+    $repository = $this->getDoctrine()->getRepository(Defile::class);
+    $queryBuilder = $repository->createQueryBuilder('d')
+        ->leftJoin('d.mannequin', 'm') // Joindre la relation mannequin
+        ->addSelect('m');
 
-        return $this->render('defile/listeDefile.html.twig', [
-            'lesDefiles' => $lesDefiles,
-        ]);
+    // Appliquer les filtres si les paramètres existent
+    if (!empty($search)) {
+        $queryBuilder->andWhere('d.nomD LIKE :search')
+            ->setParameter('search', '%' . $search . '%');
     }
-}
-    
 
+    if ($mannequin) {
+        $queryBuilder->andWhere('m.id = :mannequin')
+            ->setParameter('mannequin', $mannequin);
+    }
+
+    if ($annee) {
+        $queryBuilder->andWhere('YEAR(d.date) = :annee')
+            ->setParameter('annee', $annee);
+    }
+
+    $lesDefiles = $queryBuilder->getQuery()->getResult();
+
+    return $this->render('defile/index.html.twig', [
+        'lesDefiles' => $lesDefiles,
+        'search' => $search,
+        'mannequin' => $mannequin,
+        'annee' => $annee,
+    ]);
+
+}
+}
