136f9a6513e8bf369583947efbd4ef3eaefe7e00
Manon <manco15@outlook.fr>
2025-01-08 12:51:26 +0100

    a

--- src/Controller/MarqueController.php
+++ src/Controller/MarqueController.php
@@ -7,7 +7,6 @@ use App\Entity\Marque;
 use App\Repository\MarqueRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
