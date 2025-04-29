a267cb3eb85171079b9896c1af464c36dec8a185
Yasmine EL HARIM <yasmine.elharim@btssio.dedyn.io>
2025-04-29 15:18:26 +0200

    yy

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -14,7 +14,7 @@ class UserRepositoryTest extends KernelTestCase
     public function testCount() {
         self::bootKernel();
         $users = self::$container->get(UserRepository::class)->count([]);
-        $this->assertEquals(10,$users);
+        $this->assertEquals(0,$users);
 
     }
 
